0b42709812eac6281a0cdf42403f82cdf39babb6
matthewjsiv <matthewjsiv@gmail.com>
2023-11-13 18:32:30 -0500

    fixed ctrl+bk

--- tmux_config/kill_bag_tmux.sh
+++ tmux_config/kill_bag_tmux.sh
@@ -1,3 +1,9 @@
-rosbag_node=$(rosnode list | grep record_); rosnode kill $rosbag_node
-tmux kill-session
-
+rosbag_node=$(rosnode list | grep record_); #rosnode kill $rosbag_node
+if [ -z "$rosbag_node" ]
+then
+  tmux kill-session
+else
+  rosnode kill $rosbag_node
+  tmux kill-session
+fi
+#tmux kill-session
